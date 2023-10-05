8d9a47e21760854e880306545f8845266aebc8aa
Martin Gustafsson <martin.gustafsson.work@gmail.com>
2023-10-05 13:40:31 +0200

    Add more tests for the mod10 and mod11 functions

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,9 +1,41 @@
 const { expect } = require('chai');
 const { mod10, mod11 } = require('../lib/utils');
 
+validMod10ExamplesFromBankgirot = [
+  '3316812057492',
+  '55555551',
+  '9912346',
+]
+
+invalidatedMod10ExamplesFromBankgirot = [
+  '3316812057490',
+  '55555550',
+  '9912340',
+]
+
+validMod11ExamplesFromBankgirot = [
+  '1912763608957',
+  '241350',
+  '324558',
+]
+
+invalidatedMod11ExamplesFromBankgirot = [
+  '1912763608950',
+  '241351',
+  '324550',
+]
+
 describe('test mod 10 method', () => {
-  it('should be true using mod10 example provided by Bankgirot', () => {
-    expect(mod10('3316812057492')).to.be.true;
+  it('should be true using examples provided by Bankgirot', () => {
+    validMod10ExamplesFromBankgirot.forEach((input) => {
+      expect(mod10(input)).to.be.true;
+    })
+  });
+
+  it('should be false when Bankgirot examples have been modified', () => {
+    invalidatedMod10ExamplesFromBankgirot.forEach((input) => {
+      expect(mod10(input)).to.be.false;
+    })
   });
 
   it('should be true when valid Swedish ssn', () => {
@@ -16,7 +48,15 @@ describe('test mod 10 method', () => {
 });
 
 describe('test mod 11 method', () => {
-  it('should be true using mod11 example provided by Bankgirot', () => {
-    expect(mod11('1912763608957')).to.be.true;
+  it('should be true using examples provided by Bankgirot', () => {
+    validMod11ExamplesFromBankgirot.forEach((input) => {
+      expect(mod11(input)).to.be.true;
+    })
+  });
+
+  it('should be false when Bankgirot examples have been modified', () => {
+    invalidatedMod11ExamplesFromBankgirot.forEach((input) => {
+      expect(mod11(input)).to.be.false;
+    })
   });
 });
